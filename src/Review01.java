e98ccceb122fd77bf56494f2de01691bad08e9b9
m-bluefairy <awim@me.com>
2023-11-12 23:04:32 +0900

    消費税の計算を再提出

--- src/Review01.java
+++ src/Review01.java
@@ -3,18 +3,18 @@ public class Review01 {
 
     public static void main(String [] args) {
 
-      //変数の宣言
       int num1 = 1500; // 商品価格を代入
 
-      //商品価格に消費税の加算結果を代入
-      int result ;
-      result = tax(num1);
-      System.out.println( num1 + "円の商品の税込価格は" + (num1 + result) + "（消費税は" + result + "円）です。");
-      }
+      double result ;
+      result = tax(num1); //消費税を代入
 
-      //消費税額の算出
-          public static int tax(int num1) {
-          int result = num1 /10;
-          return result;
-      }
+      System.out.println( num1 + "円の商品の税込価格は" + (num1 + result + "円") + ("（消費税は" + result + "円）です。"));
+    }
+
+    //消費税額の算出
+    public static double tax(int num1) {
+        double result = num1 * 0.1;
+        return result;
+    }
+
 }
